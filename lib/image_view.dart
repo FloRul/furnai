0f0ed0ec13e87b0fdaa6ac0eb6070fc30045f2c3
FloRul <rumiel.florian@gmail.com>
2023-08-03 16:23:57 -0400

    first commit

---
+++ lib/image_view.dart
@@ -0,0 +1,101 @@
+﻿import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+class ImageViewPage extends StatefulWidget {
+  const ImageViewPage({super.key, required this.imagePath});
+  final String imagePath;
+
+  @override
+  State<ImageViewPage> createState() => _ImageViewPageState();
+}
+
+class _ImageViewPageState extends State<ImageViewPage> {
+  late ui.Image original;
+  bool isImageLoaded = false;
+  List<Offset?> _points = [];
+  @override
+  void initState() {
+    _loadImage();
+    super.initState();
+  }
+
+  _loadImage() async {
+    final ui.Image image = await decodeImageFromList(
+      await File(widget.imagePath).readAsBytes(),
+    );
+    setState(() {
+      isImageLoaded = true;
+      original = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: isImageLoaded
+            ? LayoutBuilder(builder: (_, constraints) {
+                return GestureDetector(
+                  onPanUpdate: (details) {
+                    setState(() {
+                      RenderBox object = _.findRenderObject() as RenderBox;
+                      Offset locationPoints = object.localToGlobal(details.globalPosition);
+                      _points = List.from(_points)..add(locationPoints);
+                    });
+                  },
+                  onPanEnd: (DragEndDetails details) {
+                    setState(() {
+                      _points.add(null);
+                    });
+                  },
+                  child: CustomPaint(
+                    size: Size(constraints.maxWidth, constraints.maxHeight),
+                    painter: ImageEditor(image: original, points: _points),
+                  ),
+                );
+              })
+            : const CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class ImageEditor extends CustomPainter {
+  // [SignaturePainter] receives points through constructor
+  // @points holds the drawn path in the form (x,y) offset;
+  // This class responsible for drawing only
+  // It won't receive any drag/touch events by draw/user.
+  List<Offset?> points = [];
+  ui.Image image;
+
+  ImageEditor({required this.points, required this.image});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.square
+      ..strokeWidth = 5.0;
+
+    final outputRect = Rect.fromPoints(ui.Offset.zero, ui.Offset(size.width, size.height));
+    final Size imageSize = Size(image.width.toDouble(), image.height.toDouble());
+    final FittedSizes sizes = applyBoxFit(BoxFit.contain, imageSize, outputRect.size);
+    final Rect inputSubrect = Alignment.center.inscribe(sizes.source, Offset.zero & imageSize);
+    final Rect outputSubrect = Alignment.center.inscribe(sizes.destination, outputRect);
+    canvas.drawImageRect(image, inputSubrect, outputSubrect, paint);
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i]!, points[i + 1]!, paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(ImageEditor oldDelegate) {
+    return oldDelegate.points != points;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:furnai/image_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> _gallery = [];
+  late Map<String, bool> _hovered;
+
+  @override
+  void initState() {
+    _hovered = Map.fromEntries(
+      _gallery.map(
+        (e) => MapEntry(e, false),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: GridView.builder(
+          itemCount: _gallery.length,
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 128),
+          itemBuilder: (_, index) => MouseRegion(
+            onHover: (event) => setState(() {
+              _hovered[_gallery[index]] = true;
+            }),
+            onExit: (event) => setState(() {
+              _hovered[_gallery[index]] = false;
+            }),
+            child: AnimatedScale(
+              scale: _hovered[_gallery[index]]! ? 1.2 : 1,
+              duration: const Duration(milliseconds: 200),
+              child: GestureDetector(
+                onTapUp: (details) => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ImageViewPage(imagePath: _gallery[index]),
+                  ),
+                ),
+                child: Image.file(
+                  File(_gallery[index]),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          FilePickerResult? result = await FilePicker.platform.pickFiles(
+            type: FileType.custom,
+            allowedExtensions: ['jpg', 'png'],
+          );
+          final path = result?.paths.firstOrNull;
+          if (path != null) {
+            setState(() {
+              _gallery.add(path);
+              _hovered.update(
+                path,
+                (value) => false,
+                ifAbsent: () => false,
+              );
+            });
+            // final cmd = img.Command()
+            //   // Decode the image file at the given path
+            //   ..decodeImageFile(path)
+            //   // Resize the image to a width of 64 pixels and a height that maintains the aspect ratio of the original.
+            //   ..copyResize(width: 64)
+            //   // Write the image to a PNG file (determined by the suffix of the file path).
+            //   ..writeToFile('thumbnail.png');
+            // // On platforms that support Isolates, execute the image commands asynchronously on an isolate thread.
+            // // Otherwise, the commands will be executed synchronously.
+            // await cmd.executeThread();
+
+            // if (cmd.outputImage != null) {
+            //   setState(() {
+            //     gallery.add(cmd.outputImage!);
+            //   });
+            // }
+          } else {
+            // ignore: use_build_context_synchronously
+            ScaffoldMessenger.maybeOf(context)?.showSnackBar(
+              const SnackBar(
+                content: Text(
+                  'File picking canceled',
+                  style: TextStyle(color: Colors.amber),
+                ),
+              ),
+            );
+          }
+        },
+        tooltip: 'Pick file',
+        child: const Icon(Icons.file_open),
+      ),
+    );
+  }
+}
